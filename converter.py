0c71da56e87d009feded50138743ab9229e33768
bahrom04 <magdiyevbahrom@gmail.com>
2025-05-22 09:31:55 +0500

    chore: add description to translation

--- converter.py
+++ converter.py
@@ -9,8 +9,9 @@ po = polib.POFile()
 for entry in data["translations"]:
     en = entry["en"]
     uz = entry["uz"]
+    description = entry["description"]
 
-    po.append(polib.POEntry(msgid=en, msgstr=uz))
+    po.append(polib.POEntry(comment=description ,msgid=en, msgstr=uz))
 
 po.save("locale/uz.po")
 
