e177a832d8955bb684552e90af5366345ef33096
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2025-06-03 22:39:03 +0900

    Add simple bash script to normalize apostrophes in .po or text files

---
+++ scripts/normalize_apostrophes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# To use with .po files use this command:
+# msgstr ./apostrophe_normalizer.sh < input.po > output.po
+
+# To use with text files use this command:
+# ./apostrophe_normalizer.sh < input.txt > output.txt
+
+sed -E \
+    -e "s/([oOgG])['‘]/\1ʻ/g" \
+    -e "s/['’]/ʼ/g"
+
