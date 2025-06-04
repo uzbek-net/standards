ce7c7f28c5d289613f08ecaad10418ebd8590590
bahrom04 <magdiyevbahrom@gmail.com>
2025-06-04 08:06:39 +0500

    feat: scripts/normalize_apostrophes.sh override existing file content without creating output file

--- scripts/normalize_apostrophes.sh
+++ scripts/normalize_apostrophes.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # To use with .po files use this command:
-# msgfilter ./apostrophe_normalizer.sh < input.po > output.po
+# ./apostrophe_normalizer.sh input.txt
 
 # To use with text files use this command:
-# ./apostrophe_normalizer.sh < input.txt > output.txt
+# ./apostrophe_normalizer.sh input.txt
+
+tmp="$(mktemp)"
 
 sed -E \
     -e "s/([oOgG])['‘]/\1ʻ/g" \
-    -e "s/['’]/ʼ/g"
-
+    -e "s/['’]/ʼ/g" \
+    "$1" > "$tmp" && mv "$tmp" "$1"
